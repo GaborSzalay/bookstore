013221c89eed547e4d8cb9bf973aff2ca5d42a9f
GaborSzalay <yaastreet@gmail.com>
2017-07-26 21:51:30 +0200

    adding form control on search

--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import 'rxjs/add/operator/startWith';
-import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'app-search',
@@ -9,71 +7,13 @@ import 'rxjs/add/operator/map';
     styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
-    stateCtrl: FormControl;
-    filteredStates: any;
-
-  states = [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming',
-  ];
+    searchControl: FormControl;
 
     constructor() {
-        this.stateCtrl = new FormControl();
-        this.filteredStates = this.stateCtrl.valueChanges
-            .startWith(null)
-            .map(name => this.filterStates(name));
+        this.searchControl = new FormControl();
+        this.searchControl.valueChanges.subscribe((value) => {
+            console.log('changed: ' + value)
+        });
     }
 
-    filterStates(val: string) {
-    return val ? this.states.filter(s => s.toLowerCase().indexOf(val.toLowerCase()) === 0)
-               : this.states;
-  }
 }
