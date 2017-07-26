9e4a708616794c372998431b71f6d4f4adf51a7a
GaborSzalay <yaastreet@gmail.com>
2017-07-26 21:12:05 +0200

    changing from material design to bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
-import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search.component';
@@ -20,9 +17,7 @@ import { SearchComponent } from './search.component';
         HttpModule,
         JsonpModule,
         FormsModule,
-        ReactiveFormsModule,
-        MaterialModule,
-        BrowserAnimationsModule
+        ReactiveFormsModule
     ],
     bootstrap: [AppComponent]
 })
