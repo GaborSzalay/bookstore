5f5242657334fa7069b8dc9c2ed32359da94b6ba
GaborSzalay <yaastreet@gmail.com>
2017-08-01 20:40:15 +0200

    lets see this variation

--- karma.conf.js
+++ karma.conf.js
@@ -1,126 +1,34 @@
-module.exports = function(config) {
+module.exports = function(config){
   config.set({
 
-    basePath: '',
+    basePath : '../',
+
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-animate/angular-animate.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
 
     frameworks: ['jasmine'],
 
-    files: [
-      // For travis
-      'node_modules/core-js/client/shim.js',
-      'node_modules/ie-shim/index.js',
-      // paths loaded by Karma
-      'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/zone.js/dist/zone.js',
-      'node_modules/zone.js/dist/long-stack-trace-zone.js',
-      'node_modules/zone.js/dist/proxy.js',
-      'node_modules/zone.js/dist/sync-test.js',
-      'node_modules/zone.js/dist/jasmine-patch.js',
-      'node_modules/zone.js/dist/async-test.js',
-      'node_modules/zone.js/dist/fake-async-test.js',
-      {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false},
-      {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false},
-      {pattern: 'karma-test-shim.js', included: true, watched: true},
-      {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
-      {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: true},
+    browsers : ['Chrome'],
 
-      // paths loaded via module imports
-      {pattern: 'temp/**/*.js', included: false, watched: true},
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
 
-      // paths to support debugging with source maps in dev tools
-      {pattern: 'src/**/*.ts', included: false, watched: false},
-      {pattern: 'temp/**/*.js.map', included: false, watched: false}
-    ],
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
 
-    preprocessors: {
-      'temp/**/*.js': 'sourcemap',
-      'temp/**/!(*.spec|*.module).js': 'coverage'
-    },
-
-    customLaunchers: {
-      'SL_CHROME': {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        version: 'latest'
-      },
-      'SL_FIREFOX': {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        version: 'latest'
-      },
-      'SL_IE10': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 2012',
-        version: '10'
-      },
-      'SL_IE11': {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
-      },
-      'SL_EDGE13': {
-        base: 'SauceLabs',
-        browserName: 'MicrosoftEdge',
-        platform: 'Windows 10',
-        version: '13.10586'
-      },
-      'SL_EDGE14': {
-        base: 'SauceLabs',
-        browserName: 'MicrosoftEdge',
-        platform: 'Windows 10',
-        version: '14.14393'
-      },
-      'SL_SAFARI9': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '9.0'
-      },
-      'SL_SAFARI10': {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '10.0'
-      }
-    },
-
-    sauceLabs: {
-      testName: 'ng-bootstrap',
-      retryLimit: 3,
-      startConnect: false,
-      recordVideo: false,
-      recordScreenshots: false,
-      options: {
-        'command-timeout': 600,
-        'idle-timeout': 600,
-        'max-duration': 5400
-      }
-    },
-
-    reporters: ['progress', 'coverage'],
-
-    coverageReporter: {
-      dir: 'coverage/',
-      reporters: [{
-        type: 'json',
-        dir: 'coverage',
-        subdir: 'json',
-        file: 'coverage-final.json'
-      }]
-    },
-
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Firefox'],
-    singleRun: false,
-    captureTimeout: 60000,
-    browserDisconnectTimeout: 60000,
-    browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 60000
   });
 };
