01b4e12c9bc2714c001922cdf7ab902a3b20260d
GaborSzalay <yaastreet@gmail.com>
2017-08-01 17:05:24 +0200

    adding app component test

--- README.md
+++ README.md
@@ -6,5 +6,9 @@ Example Angualar app that fetches and stores books [![Circle CI](https://circlec
 3. run `yarn start`
 4. open `http://localhost:4200/`
 
+# Running tests
+1. run `yarn install`
+3. run `yarn test`
+
 Written in Angular and TypeScript.
 Still in progress, see [issues](https://github.com/GaborSzalay/bookstore/issues) for the upcoming features and bugs.
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RouterOutletStubComponent } from './testing/router-stubs';
+
+let comp: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+
+describe('AppComponent & TestModule', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent, RouterOutletStubComponent
+            ]
+        })
+            .compileComponents()
+            .then(() => {
+                fixture = TestBed.createComponent(AppComponent);
+                comp = fixture.componentInstance;
+            });
+    }));
+
+    it('can instantiate it', () => {
+        expect(comp).not.toBeNull();
+    });
+
+    it('can find router-outlet on template', () => {
+        const routerOutletDirective = fixture.debugElement.queryAll(By.directive(RouterOutletStubComponent));
+        expect(routerOutletDirective[0].name).toEqual('router-outlet');
+    });
+});
